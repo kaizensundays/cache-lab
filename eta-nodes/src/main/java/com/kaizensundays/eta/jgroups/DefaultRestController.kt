0c21b8759e3c9930de59fa52f8e329b97068dcbb
KaizenSundays <kaizensundays@localhost>
2025-01-25 19:55:02 -0500

    [ eta-node ] shutdown

--- eta-nodes/src/main/java/com/kaizensundays/eta/jgroups/DefaultRestController.kt
+++ eta-nodes/src/main/java/com/kaizensundays/eta/jgroups/DefaultRestController.kt
@@ -2,9 +2,16 @@ package com.kaizensundays.eta.jgroups
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
+import java.lang.Thread.sleep
+import kotlin.system.exitProcess
 
 /**
  * Created: Sunday 9/29/2024, 12:28 PM Eastern Time
@@ -14,12 +21,32 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class DefaultRestController(
     private val cacheCommandHandler: CacheCommandHandler
-) {
+) : ApplicationContextAware {
 
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
+    private lateinit var context: ConfigurableApplicationContext
+
+    override fun setApplicationContext(context: ApplicationContext) {
+        this.context = context as ConfigurableApplicationContext
+    }
+
     @GetMapping("/ping")
     fun ping(): String {
+        return "Ok"
+    }
+
+    @GetMapping("/shutdown")
+    fun shutdown(): String {
+        logger.info("Shutdown")
+
+        Mono.fromRunnable<Any> {
+            context.close()
+            logger.info("Exiting JVM ...")
+            sleep(3000)
+            exitProcess(0)
+        }.subscribeOn(Schedulers.boundedElastic()).subscribe()
+
         return "Ok"
     }
 
