50c2f6b45a2017eb4a8db6dd2ff7af70fd07b212
KaizenSundays <kaizensundays@localhost>
2025-02-09 14:39:56 -0500

    [ eta-nodes ] CacheRemoteTest

---
+++ eta-nodes/src/test/java/com/kaizensundays/eta/jgroups/CacheRemoteTest.kt
@@ -0,0 +1,16 @@
+package com.kaizensundays.eta.jgroups
+
+import org.junit.jupiter.api.Test
+
+/**
+ * Created: Sunday 2/9/2025, 12:36 PM Eastern Time
+ *
+ * @author Sergey Chuykov
+ */
+class CacheRemoteTest {
+
+    @Test
+    fun test() {
+    }
+
+}
